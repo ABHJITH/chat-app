19ee8df32154e10b5b1e5090f74e29710331a79d
abhijithsurendran6 <abhijithsurendran6@gmail.com>
2020-08-22 02:30:18 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyD7ZyGN4-MWLT3uPWBNjvCh8x9_sRY0aas',
+  databaseURL: 'https://chat-test-95ef2.firebaseio.com'
+};
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'chat-app';
+  title = 'angular-chat';
+
+  constructor() {
+    firebase.initializeApp(config);
+  }
 }
